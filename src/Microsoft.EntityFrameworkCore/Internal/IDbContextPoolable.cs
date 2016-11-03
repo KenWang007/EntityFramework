8fa937c20f83d929a16b563608cb45242d71412e
Andrew Peters <anpete@microsoft.com>
2016-11-03 10:32:46 -0700

    PERF: Fix #6923 - Implement DbContext pooling for high scale scenarios.
    - Initial impl. of context pooling.

---
+++ src/Microsoft.EntityFrameworkCore/Internal/IDbContextPoolable.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.EntityFrameworkCore.Internal
+{
+    /// <summary>
+    ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public interface IDbContextPoolable
+    {
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        IDbContextPool DbContextPool { get; set; }
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        DbContextPoolConfigurationSnapshot SnapshotConfiguration();
+
+        /// <summary>
+        ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        void Resurrect(DbContextPoolConfigurationSnapshot configurationSnapshot);
+    }
+}
